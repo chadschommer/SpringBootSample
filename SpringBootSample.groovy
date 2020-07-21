84b43ad35243ba52fe050de1ea6235092676d5ae
Chad Schommer <chad.schommer@peoplefluent.com>
2020-07-21 13:49:17 -0500

    squashme

--- SpringBootSample.groovy
+++ SpringBootSample.groovy
@@ -33,6 +33,7 @@ podTemplate(label: label, containers: [
                 }
 
                 docker.withRegistry('https://registry.hub.docker.com', 'docker-registry-personal') {
+                    def app
                     app = docker.build("schommer21/springboot-sample")
                     app.push("${env.BUILD_NUMBER}"
                     app.push("latest")
