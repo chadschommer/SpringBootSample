661c6c76550f84875cdabf753241e3edbd83ba87
Chad Schommer <chad.schommer@peoplefluent.com>
2020-07-21 13:57:57 -0500

    wrap commands in docker login

--- SpringBootSample.groovy
+++ SpringBootSample.groovy
@@ -20,10 +20,12 @@ podTemplate(label: label, containers: [
         stage('Build Container') {
             container('docker-build') {
 
-                git url: repo, credentialsId: 'personal-ssh-github', branch: 'master'
-                withCredentials([sshUserPrivateKey(credentialsId: 'personal-ssh-github', keyFileVariable: 'GIT_KEY')]) {
-                    withEnv(["GIT_SSH_COMMAND=ssh -i $GIT_KEY -o StrictHostKeyChecking=no"]) {
-                        dockerbuild()
+                docker.withRegistry('https://registry.hub.docker.com', 'docker-registry-personal') {
+                    git url: repo, credentialsId: 'personal-ssh-github', branch: 'master'
+                    withCredentials([sshUserPrivateKey(credentialsId: 'personal-ssh-github', keyFileVariable: 'GIT_KEY')]) {
+                        withEnv(["GIT_SSH_COMMAND=ssh -i $GIT_KEY -o StrictHostKeyChecking=no"]) {
+                            dockerbuild()
+                        }
                     }
                 }
             }
