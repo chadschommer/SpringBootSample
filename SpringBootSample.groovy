f9c9c7b216282034967845abdc474f5b3f36ee5a
Chad Schommer <chad.schommer@peoplefluent.com>
2020-07-21 09:46:51 -0500

    initial jenkinsfile

---
+++ SpringBootSample.groovy
@@ -0,0 +1,31 @@
+import groovy.json.JsonSlurper
+import java.text.SimpleDateFormat
+
+def branch = "master"
+def repo = "git@github.com:schommer21/SpringBootSample.git"
+def label = "worker-${UUID.randomUUID().toString()}"
+
+def dockerbuild() {
+    sh """
+    docker build --no-cache --network=host -t schommer21/springboot-sample:dev -f Dockerfile .
+    docker push schommer21/springboot-sample:dev
+    """
+}
+
+podTemplate(label: label, containers: [
+        containerTemplate(name: 'docker-build', image: '816004290214.dkr.ecr.us-east-1.amazonaws.com/docker-build', command: 'cat', ttyEnabled: true, resourceRequestCpu: '50m', resourceLimitCpu: '100m', resourceRequestMemory: '500Mi', resourceLimitMemory: '1000Mi')
+],
+        volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')]
+) {
+    node(label) {
+        stage('Build Container') {
+            container('docker-build') {
+                git url: repo, credentialsId: 'personal-github', branch: 'master' {
+                    withEnv(["GIT_SSH_COMMAND=ssh -i $GIT_KEY -o StrictHostKeyChecking=no"]) {
+                        dockerbuild()
+                    }
+                }
+            }
+        }
+    }
+}
